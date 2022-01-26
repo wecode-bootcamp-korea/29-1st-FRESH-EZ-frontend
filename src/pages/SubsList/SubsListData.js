eb3f5056ccf9eba5a24e4ad882bad0621675717f
pkw-May <pkw.may@gmail.com>
2022-01-27 03:10:59 +0900

    Add: images for SubsList

---
+++ src/pages/SubsList/SubsListData.js
@@ -0,0 +1,32 @@
+const SubsListData = [
+  {
+    id: 1,
+    prod: '샐러드',
+    desc: '식단 관리의 시작은 역시',
+    price_info: '2주 구독 36,000원부터',
+    img_url: 'https://www.flaticon.com/authors/flatart-icons',
+  },
+  {
+    id: 2,
+    prod: '샌드위치',
+    desc: '간편과 건강을 한 입에',
+    price_info: '2주 구독 37,000원부터',
+    img_url: 'https://www.instagram.com/moreatheyoung/',
+  },
+  {
+    id: 3,
+    prod: '도시락',
+    desc: 'No밥 No라이프, K-식단러를 위한',
+    price_info: '2주 구독 38,000원부터',
+    img_url: 'https://github.com/pkw-May',
+  },
+  {
+    id: 4,
+    prod: '반반메뉴',
+    desc: '이왕이면 다채롭게',
+    price_info: '2주 구독 39,000원부터',
+    img_url: 'https://github.com/pkw-May',
+  },
+];
+
+export default SubsListData;
