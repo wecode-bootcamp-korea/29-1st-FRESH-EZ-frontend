e4ff4c9ec2dfb0fec381a20cb4cf0e6aa56c9384
pkw-May <pkw.may@gmail.com>
2022-01-27 05:03:59 +0900

    Add: Subscribe Product List Components & Apply style

--- src/pages/SubsList/SubsList.js
+++ src/pages/SubsList/SubsList.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import SubsProd from './SubsProd';
+import SubsListData from './SubsListData';
 import './SubsList.scss';
 
 function SubsList() {
-  return <div>test 테스트</div>;
+  return (
+    <ul className="SubsList">
+      <div className="SubsListHead">
+        <p className="title">구독 상품</p>
+        <p className="SubsSlogan">
+          <span>구독 횟수부터 식단까지 모두</span>
+          <span> 내 맘대로</span>
+        </p>
+      </div>
+      {SubsListData.map(data => {
+        return (
+          <SubsProd
+            key={data.id}
+            prod={data.prod}
+            desc={data.desc}
+            price_info={data.price_info}
+            img_url={data.img_url}
+            img_pos={data.img_pos}
+          />
+        );
+      })}
+    </ul>
+  );
 }
 
 export default SubsList;
--- src/pages/SubsList/SubsProd.js
+++ src/pages/SubsList/SubsProd.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import './SubsProd.scss';
 
-// 버튼 배경 이미지를 넣으면, 반응형 사이즈 조절할때 비율 깨지는 현상 발생 가능하니..
-// 그냥 간단한 아이콘 이미지를 div로 얹기!
-// 창을 완전히 줄이면 없어지고, 텍스트만 남게!
-
-function SubsProd() {
+function SubsProd({ prod, desc, price_info, img_url, img_pos }) {
   return (
-    <div className="SubsProd">
-      <div />
-      <div>
-        <p className="SubProdName">{props.name}</p>
-        <p className="">{props.name}</p>
+    <li className="SubsProd" position={img_pos}>
+      <img className="SubsProdImg" alt={prod} src={img_url} />
+      <div className="SubsProdText">
+        <div className="SubsProdMain">
+          <p className="SubsProdName">
+            {prod}
+            {' >'}
+          </p>
+          <p className="SubsProdDesc">{desc}</p>
+        </div>
+        <div className="SubsProdSub">{price_info}</div>
       </div>
-    </div>
+    </li>
   );
 }
 
