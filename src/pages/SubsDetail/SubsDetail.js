fcff4bef951385e786fbb6c599f8a1d6f0bf3baf
hyunkark <hyunkark@gmail.com>
2022-01-28 21:50:09 +0900

    Add : SubsDetail.js JSX코드를 추가함

--- src/pages/SubsDetail/SubsDetail.js
+++ src/pages/SubsDetail/SubsDetail.js
@@ -9,8 +9,19 @@ function SubsDetail() {
           alt="salad"
           src="https://play-lh.googleusercontent.com/IeNJWoKYx1waOhfWF6TiuSiWBLfqLb18lmZYXSgsH1fvb8v1IYiZr5aYWe0Gxu-pVZX3"
         />
-        <section>
+        <section className="information">
           <div>샐러드 정기구독</div>
+          <div>[기간] 1주-8주 / [배송] 주 1회-6회 / [사이즈] M/L Size</div>
+          <div>
+            <i class="fas fa-star" />
+            <i class="fas fa-star" />
+            <i class="fas fa-star" />
+            <i class="fas fa-star" />
+            <i class="fas fa-star-half" />
+            4.8(후기 3,522)
+          </div>
+          <div>2주 6회</div>
+          <div>36,000원 ~</div>
         </section>
       </header>
       <article>
