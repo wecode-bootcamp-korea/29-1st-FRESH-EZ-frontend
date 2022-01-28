babe553759d33752d060fb6303c03d8902fa4907
hyunkark <hyunkark@gmail.com>
2022-01-28 10:00:27 +0900

    Add : 구독 상세 페이지의 js, scss파일을 추가했습니다

---
+++ src/pages/SubsDetail/SubsDetail.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './SubsDetail.scss';
+
+function SubsDetail() {
+  return (
+    <div class="SubsDetail">
+      <main>
+        <img
+          alt="salad"
+          src="https://play-lh.googleusercontent.com/IeNJWoKYx1waOhfWF6TiuSiWBLfqLb18lmZYXSgsH1fvb8v1IYiZr5aYWe0Gxu-pVZX3"
+        />
+        <section>
+          <div>Title</div>
+        </section>
+        <section>
+          <img
+            alt="product details"
+            src="https://infographicjournal.com/wp-content/uploads/2013/01/hidden-value-of-long-tail-seo-10001.png"
+          />
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default SubsDetail;
