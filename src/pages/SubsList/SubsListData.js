747879eb7959d5275371078aced65102cdf8cf80
pkw-May <pkw.may@gmail.com>
2022-01-30 19:02:42 +0900

    Modify: added url address of detail pages

--- src/pages/SubsList/SubsListData.js
+++ src/pages/SubsList/SubsListData.js
@@ -1,6 +1,7 @@
 const SubsListData = [
   {
     id: 1,
+    category: 'Salad',
     prod: '샐러드',
     desc: '식단 관리의 시작은 역시',
     price_info: '2주 구독 36,000원 ~',
@@ -9,6 +10,7 @@ const SubsListData = [
   },
   {
     id: 2,
+    category: 'Sandwich',
     prod: '샌드위치',
     desc: '간편과 건강을 한 입에',
     price_info: '2주 구독 37,000원 ~',
@@ -17,6 +19,7 @@ const SubsListData = [
   },
   {
     id: 3,
+    category: 'Bento',
     prod: '도시락',
     desc: '식단해도 밥은 못 잃어',
     price_info: '2주 구독 38,000원 ~',
@@ -25,6 +28,7 @@ const SubsListData = [
   },
   {
     id: 4,
+    category: 'Half',
     prod: '반반메뉴',
     desc: '이왕이면 다채롭게',
     price_info: '2주 구독 39,000원 ~',
