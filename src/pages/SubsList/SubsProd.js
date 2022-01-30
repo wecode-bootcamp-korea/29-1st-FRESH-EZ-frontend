352075b7e2821f0ef1266ed1b0a4bb0bf9f09f61
pkw-May <pkw.may@gmail.com>
2022-01-30 19:05:04 +0900

    Add: added animation effects on mouse over action

--- src/pages/SubsList/SubsProd.js
+++ src/pages/SubsList/SubsProd.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './SubsProd.scss';
 
 function SubsProd({ data }) {
-  const { prod, desc, price_info, img_url, img_pos } = data;
+  const { category, prod, desc, price_info, img_url, img_pos } = data;
+  const [buttonMode, setButtonMode] = useState('Default');
+  const navigate = useNavigate();
+  function handleClick() {
+    navigate(`/subsDetail${category}`);
+  }
+  function emphasizeButton() {
+    setButtonMode('Emphasized');
+  }
+  function resetButton() {
+    setButtonMode('Default');
+  }
 
   return (
-    <li className="subsProd" position={img_pos}>
+    <li
+      className={`subsProd${buttonMode}`}
+      position={img_pos}
+      onClick={handleClick}
+      onMouseEnter={emphasizeButton}
+      onMouseLeave={resetButton}
+    >
       <img className="subsProdImg" alt={prod} src={img_url} />
       <div className="subsProdText">
         <div className="subsProdMain">
