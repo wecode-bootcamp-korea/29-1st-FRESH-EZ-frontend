e72ca2198420b06312cbe072d4ce96a48751f947
이주미 <jumi@gyeongmin-ui-MacBookAir.local>
2022-02-01 00:52:46 +0900

    레이아웃 css 뼈대만 만들어놓음

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
+import Footer from './components/Footer/Footer';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/main" element={<Main />} />
+        <Route path="/footer" element={<Footer />} />
       </Routes>
     </BrowserRouter>
   );
