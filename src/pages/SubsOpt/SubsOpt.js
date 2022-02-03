f59acbc06f0e25bf2d86268bf7fb0c1b1aa78d34
pkw-May <pkw.may@gmail.com>
2022-02-04 03:21:12 +0900

    Add: preprocessing module for posting user selected datas to server

--- src/pages/SubsOpt/SubsOpt.js
+++ src/pages/SubsOpt/SubsOpt.js
@@ -4,14 +4,17 @@ import SubsOptForm from './SubsOptForm';
 import SubsStep from './SubsStep';
 import SubsOptQuery from './SubsOptQuery';
 import SubsOptSelect from './SubsOptSelect';
-import SubsOptPrice from './SubsOptPrice';
+import SubsOptPred from './SubsOptPred';
 import SubsOptStepBtn from './SubsOptStepBtn';
 import SubsOptData from './SubsOptData';
 import './SubsOpt.scss';
 
-// modal창에서 옵션 선택 안 한 경우에도 esc 키 누를때 모달창 꺼지는 기능
+// modal창에서 옵션 선택 안 한 경우에도 esc 키 누를때 모달창 꺼지는 기능 추가
 
 function SubsOpt({ modalState, closeModal }) {
+  const testJwt =
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6M30.niNmVRTCz9yD_B5BjBhfyVaDVAKyboSxAUxRmOOnvJg';
+  const prodCategory = '1';
   const navigate = useNavigate();
   const [step, setStep] = useState(
     () => Number(window.localStorage.getItem('step')) || 1
@@ -22,8 +25,6 @@ function SubsOpt({ modalState, closeModal }) {
   }, [step]);
 
   const [selectedData, setSelectedData] = useState({
-    category_id: 1,
-    email: 'aaa@gmail.com',
     size: 'Medium',
     food_day_count: '1회',
     food_week_count: '주 3일',
@@ -48,36 +49,37 @@ function SubsOpt({ modalState, closeModal }) {
     window.localStorage.setItem('selectedData', JSON.stringify(selectedData));
   }, [selectedData]);
 
+  // 나중에 local storage에서 ctegory_id, jwt 받아와서 넣어줘야함
+  // client 단에서 preprocessing하는 것 vs server에서 받을 때 하는 것 효율?
+  const preprocessUserData = userData => {
+    let sizeNum = userData.size === 'Medium' ? '1' : '2';
+    return {
+      ...userData,
+      category_id: prodCategory,
+      jwt: testJwt,
+      size: sizeNum,
+      food_day_count: /\d/.exec(userData.food_day_count).toString(),
+      food_week_count: /\d/.exec(userData.food_week_count).toString(),
+      food_period: /\d/.exec(userData.food_period).toString(),
+    };
+  };
+
   const moveToBasket = () => {
     alert('장바구니로 이동하시겠습니까?');
     navigate('/basket');
-    console.log(window.localStorage.getItem('selectedData'));
     fetch('http://208.82.62.99:8000/product/subscribe-option', {
       method: 'post',
-      body: JSON.stringify({
-        category_id: '1',
-        email: 'aaa@gmail.com',
-        size: '1',
-        food_day_count: '1',
-        food_week_count: '3',
-        food_period: '2',
-        food_start: [
-          new Date().getFullYear(),
-          (new Date().getMonth() + 1).toString().padStart(2, '0'),
-          new Date().getDate().toString().padStart(2, '0'),
-        ].join('-'),
-      }),
+      body: JSON.stringify(preprocessUserData(selectedData)),
     });
   };
 
   const moveToNext = () => {
     alert('식단 구성 단계로 넘어가시겠습니까?');
     navigate('/subsOptSelf');
-    console.log(window.localStorage.getItem('selectedData'));
     fetch('http://208.82.62.99:8000/product/subscribe-detail/1')
       .then(res => res.json())
       .then(res => {
-        console.log(`${res} ----> 잘나왔음!`);
+        window.localStorage.setItem('productsInfo', res.products);
       });
   };
 
@@ -101,8 +103,8 @@ function SubsOpt({ modalState, closeModal }) {
             moveToNext={moveToNext}
           />
         ))}
-        subsOptPrice={
-          <SubsOptPrice
+        SubsOptPred={
+          <SubsOptPred
             queryKey={data.selectOpt.queryKey}
             selectedValue={selectedData[data.selectOpt.queryKey]}
             selectedData={selectedData}
