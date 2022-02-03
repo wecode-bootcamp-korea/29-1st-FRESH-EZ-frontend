31efda4fd60d43fd94c5157e44fe9ecf475c1f27
이주미 <jumi@gyeongmin-ui-MacBookAir.local>
2022-02-03 12:24:57 +0900

    메인 배너 슬라이딩 구현 시작

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import Nav from '../../components/Nav/Nav';
+import './Main.scss';
 
 function Main() {
-  return <div>메인 페이지</div>;
+  return (
+    <>
+      <Nav />
+      <div className="main">
+        <div className="container">
+          <img
+            src="https://images.unsplash.com/photo-1638913658642-8f22bae3335b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+            alt="잠시만 기다려주세요"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1643747494952-6d70ab0cb511?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
+            alt="잠시만 기다려주세요"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1643755027544-eee0a8690ea7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+            alt="잠시만 기다려주세요"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1643754713080-cbfc5be44520?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+            alt="잠시만 기다려주세요"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Main;
